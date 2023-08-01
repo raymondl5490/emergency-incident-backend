c38756f688756ef1fbd936aea6f34caf06b6660b
Raymond <raymondl5490@gmail.com>
2023-08-01 10:33:11 -0700

    enrich incident with weather data

--- services/incidentService.js
+++ services/incidentService.js
@@ -1,4 +1,6 @@
 import { importIncidentData } from '../helpers/dataImportHelper';
+import { getWeatherData } from './weatherService';
+import dayjs from '../helpers/dayjs'
 
 export const getIncidentData = () => {
     const incident1 = importIncidentData('F01705150050.json');
@@ -7,5 +9,22 @@ export const getIncidentData = () => {
 };
 
 export const enrichIncidentData = async (incidents) => {
-    return {};
+
+    const enrichedIncidents = await Promise.all(incidents.map(async (incident) => {
+        const { latitude, longitude } = incident.address;
+        const { event_opened } = incident.description;
+    
+    
+        const dateObj = dayjs(event_opened);
+    
+        const date = dateObj.utc().format('YYYY-MM-DD');
+        const time = parseInt(dateObj.utc().format('HH'));
+
+        const weatherData = await getWeatherData(latitude, longitude, date, time);
+    
+        incident.weather = weatherData;
+        return incident;
+    }))
+
+    return enrichedIncidents;
 };
