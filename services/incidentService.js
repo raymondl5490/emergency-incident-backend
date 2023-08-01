73b5e064386d56dc287c923c62c8da67ec33c383
Raymond <raymondl5490@gmail.com>
2023-08-01 10:26:32 -0700

    return raw IncidentData from .json

--- services/incidentService.js
+++ services/incidentService.js
@@ -1,5 +1,9 @@
+import { importIncidentData } from '../helpers/dataImportHelper';
+
 export const getIncidentData = () => {
-    return [{}, {}];
+    const incident1 = importIncidentData('F01705150050.json');
+    const incident2 = importIncidentData('F01705150090.json');
+    return [incident1, incident2];
 };
 
 export const enrichIncidentData = async (incidents) => {
